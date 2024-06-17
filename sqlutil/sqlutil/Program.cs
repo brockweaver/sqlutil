e8a89eec530f8b57d4a32c6333e175c92592c868
brockweaver <brockweaver@gmail.com>
2024-06-17 08:24:51 -0500

    command help verbiage changes. copy now wipes prior to import

--- sqlutil/sqlutil/Program.cs
+++ sqlutil/sqlutil/Program.cs
@@ -181,7 +181,11 @@ public static class Program
                 Data.Export(sourceDb, filePath);
                 Console.WriteLine($"==> Done exporting data from {sourceDb} to {filePath}.");
 
-                Console.WriteLine($"==> Begin importing data from {filePath} to {targetDb} ...");
+                Console.WriteLine($"=> Begin wiping data from {targetDb} ...");
+                Data.Wipe(targetDb);
+                Console.WriteLine($"==> Done wiping data from {targetDb}.");
+
+                Console.WriteLine($"=> Begin importing data from {filePath} to {targetDb} ...");
                 Data.Import(filePath, targetDb);
                 Console.WriteLine($"==> Done importing data from {filePath} to {targetDb}.");
             }
@@ -205,24 +209,26 @@ public static class Program
         Console.WriteLine(@"
 sqlutil <cmd> [options]
     export [source_db] [output_file_path]
-        source_db = [connections.json keyname for source_db | full connection string for source_db]
         copies all data from source_db and writes into file at output_file_path
+        source_db = [key_name | full_connection_string]
 
     import [input_file_path] [target_db]
-        target_db = [connections.json keyname for target_db | full connection string for target_db]
         copies data from file at input_file_path and inserts it into the database at target_db
         will do so in a foreign-key aware order, so FK constraints do not need to be disabled or ignored
+        target_db = [key_name | full_connection_string]
 
     copy [source_db] [target_db]
-        source_db = [connections.json keyname for source_db | full connection string for source_db]
-        target_db = [connections.json keyname for target_db | full connection string for target_db]
         copies data from source_db to target_db
         this is the same as running the following commands:
             sqlutil export [source_db] dumpfile.txt data
+            sqlutil wipe [target_db]
             sqlutil import dumpfile.txt [target_db]
+        source_db = [key_name | full_connection_string]
+        target_db = [key_name | full_connection_string]
+
     wipe [target_db]
-        target_db = [connections.json keyname for target_db | full connection string for target_db]
         deletes all data in the database in proper FK order and leaves schema intact
+        target_db = [key_name | full_connection_string]
 
     add [key_name] [full_connection_string]
         adds new entry to connections.json with given key name and value.
@@ -231,7 +237,7 @@ sqlutil <cmd> [options]
     remove [key_name]
         removes given entry from connections.json. does not touch corresponding database.
 
-    test [key_name | full connection string to db]
+    test [key_name | full_connection_string]
         tests connection to db, either by looking up its key_name or using the given string as a connection string
 
     list
